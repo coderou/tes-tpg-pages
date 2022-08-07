f37acf2458c0d12b568533bd0f02ceb543315d11
coderou <13739477489@qq.com>
2022-08-07 16:45:48 +0800

    修改publicPath配置

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  publicPath: "/tes-tpg-pages/", // 解决gh-pages访问不到静态资源问题
 });
